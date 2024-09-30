6e7259b132cb15c7fb805bd01102bb2d6625c745
Animesh Singh <iamanimesh11june@gmail.com>
2024-09-30 15:13:58 +0530

    u

--- pyspark/pyspark.py
+++ pyspark/pyspark.py
@@ -1,12 +1,1 @@
-# Import findspark
-import findspark
-findspark.init()
-from pyspark.sql import  row
-row = Row(name='q', age=25, city='India')
-
-# Access the values of the row using dot notation
-print(row.name)
-print(row.age)
-print(row.city)
-
-
+print("hello")
--- pyspark/scrap.py
+++
@@ -1,8 +0,0 @@
-from typing import Optional
-
-def find_node(node: Optional[ListNode], value: int) -> Optional[ListNode]:
-    if node is None:
-        return None
-    if node.value == value:
-        return node
-    return find_node(node.next, value)
