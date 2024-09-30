d9c3fe95e11b29caa4236ff024306d1f2f9d08c5
Animesh Singh <iamanimesh11june@gmail.com>
2024-09-30 17:33:37 +0530

    u

--- pyspark/p_pyspark.py
+++ pyspark/p_pyspark.py
@@ -1,16 +1,14 @@
-#
-# from pyspark.sql import SparkSession
-# spark=SparkSession.builder.master("local").appName("first pyspark app").getOrCreate()
-#
-# sc=spark.sparkContext
-#
-# data=[1,2,3,4,5]
-# rdd=sc.parallelize(data)
-#
-# rdd_transformed =rdd.map(lambda x: x*2)
-# result=rdd_transformed.collect()
-# print("tranformed: ",result)
-#
-# spark.stop()
 
 from pyspark.sql import SparkSession
+spark=SparkSession.builder.master("local").appName("first pyspark app").getOrCreate()
+
+sc=spark.sparkContext
+
+data=[1,2,3,4,5]
+rdd=sc.parallelize(data)
+
+rdd_transformed =rdd.map(lambda x: x*2)
+result=rdd_transformed.collect()
+print("tranformed: ",result)
+spark.stop()
+
