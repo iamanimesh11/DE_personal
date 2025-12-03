d93704827b1e9653121479aa4a1ceff6f423ef5d
Animesh Singh <122896955+iamanimesh11@users.noreply.github.com>
2025-12-03 09:17:26 +0530

    Update and rename linked_list to linked_list.py

---
+++ notes/DSA/linked_list.py
@@ -0,0 +1,161 @@
+class Node:
+    def __init__(self,data):
+        self.data=data
+        self.next=None
+
+class LinkedList:
+    def __init__(self):
+        self.head=None
+        
+    def insert_end(self,data):
+        new_node=Node(data)
+        
+        if self.head is None:
+            print("yes self.head is none")
+            self.head=new_node
+            return
+
+           
+        print(f"in class node : {self.__dict__}")
+        temp=self.head
+        print(f" temp var is {temp.__dict__}")
+        while temp.next: 
+            print("im in while loop")  
+            temp=temp.next
+        print(f"temp.next is {temp.next}")
+        temp.next=new_node
+        print(f"temp.next is {temp.next.__dict__}")
+    
+   
+    def insert_atINDEX(self,index,data):
+        new_node=Node(data)
+        
+        if index==0:
+            print("im in index 0")
+            print(f"self.head.next is {self.head.next.__dict__}")
+            new_node.next=self.head
+            self.head=new_node
+            return 
+
+
+        temp=self.head
+        print(f"temp is {temp.__dict__}")
+        curr_index=0
+
+        while temp is not None and curr_index<index-1:
+                    temp=temp.next
+                    curr_index+=1
+            
+        if temp is None:
+            raise IndexError("index out of range")
+
+        new_node.next=temp.next
+        temp.next=new_node
+
+    def delete_AtINDEX(self,index):
+        if self.head is None:
+            return IndexError("list is empty")
+
+        if  index==0:
+            self.head=self.head.next
+
+        temp=self.head
+        curr_index=0
+
+        while temp is not None and curr_index<index-1:
+            temp =temp.next
+            curr_index+=1
+
+        if temp is  None or temp.next is  None:
+            raise IndexError("index out of unbound")
+
+        temp.next=temp.next.next
+        
+        
+    def print_list(self):
+        temp=self.head
+        while temp:
+            print(temp.data,end=" ")
+            temp=temp.next
+        print()
+
+    def count_nodes(self):
+        count=0
+        temp=self.head
+        while temp:
+            count+=1
+            temp=temp.next
+        print(f"count is {count}")
+
+    def count_node_recursively(self,node):
+        if node is None:
+               return 0
+        return 1+self.count_node_recursively(node.next)
+            
+
+    def search(self,target):
+        temp=self.head
+
+        while temp:
+            if temp.data==target:
+                print(f"target {target} exist in LL")
+                return
+            temp=temp.next
+        print(f"target {target} doesnt exist in LL")
+        return 
+        
+    def find_middle(self):
+            slow=self.head
+            fast=self.head
+
+            while fast and  fast.next:
+                slow =slow.next
+                fast=fast.next.next
+            return slow
+
+    def reverse_ll(self):
+      curr=self.head
+      prev=None
+      print("#############3loop starts#######")
+      while curr:
+          next=curr.next
+          curr.next=prev
+          prev=curr
+          curr=next
+  
+      self.head=prev    
+    
+    def reverse_ll_recursive(self):
+      self.head=self._reverse_Recursive(self.head)
+    
+    def _reverse_Recursive(self,node):
+      if node is None or node.next is None:
+        return node
+      print(f"node is {node.__dict__}")
+      new_head=self._reverse_Recursive(node.next)
+      node.next.next =node
+      node.next=None
+      return new_head
+  
+          
+      
+ll=LinkedList()
+ll.print_list()
+print("###### new func calling again ###")
+ll.insert_end(4)
+ll.insert_end(3)
+ll.insert_end(2)
+ll.insert_end(1)
+# ll.insert_atINDEX(4,50)
+ll.print_list()
+count=ll.count_node_recursively(ll.head)
+print(f"count is :{count}")
+# ll.delete_AtINDEX(1)
+ll.print_list()
+middle=ll.find_middle()
+print(f"middle is : {middle.data}")
+ll.reverse_ll()
+ll.print_list()
+ll.reverse_ll_recursive()
+ll.print_list()
+
